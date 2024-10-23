c231154b369409f5404214a943229e90f9f8e926
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2024-10-23 22:26:27 +0600

    display data from json

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,29 @@
 import PropTypes from 'prop-types';
 
-const Blog = ({blog}) => {
-    console.log(blog)
+const Blog = ({ blog }) => {
+    const { title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover picture of title ${title}`} className='w-full h-[500px] object-cover rounded-md' />
+            <div className='flex justify-between'>
+                <div className='flex gap-6'>
+                    <img src={author_img} alt="" className='w-14 rounded-full' />
+                    <div>
+                        <h4 className="text-2xl">{author}</h4>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h3 className="text-3xl">{title}</h3>
+            <p>
+                {
+                    hashtags.map( (hash, idx) => <span key={idx}><a href=""> #{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
